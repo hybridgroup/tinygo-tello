6a268583d8cbd932881687a55f58288eceb082b2
deadprogram <ron@hybridgroup.com>
2022-01-23 23:17:40 +0100

    examples: add basic takeoff/landing example
    Signed-off-by: deadprogram <ron@hybridgroup.com>

---
+++ examples/takeoff/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"time"
+
+	tello "github.com/hybridgroup/tinygo-tello"
+)
+
+// Tello drone info here
+const ssid = ""
+const pass = ""
+
+var (
+	drone *tello.Tello
+)
+
+func main() {
+	a := initAdaptor()
+	drone = tello.New(a, "8888")
+
+	connectToAP(connectDrone)
+}
+
+func connectDrone() {
+	println("Starting drone")
+	drone.Start()
+
+	time.Sleep(5 * time.Second)
+
+	println("Taking off")
+	drone.TakeOff()
+	time.Sleep(5 * time.Second)
+
+	println("Landing")
+	drone.Land()
+}
+
+// connect to access point
+func connectToAP(connectHandler func()) {
+	time.Sleep(2 * time.Second)
+	println("Connecting to " + ssid)
+	err := adaptor.ConnectToAccessPoint(ssid, pass, 10*time.Second)
+	if err != nil { // error connecting to AP
+		for {
+			println(err)
+			time.Sleep(1 * time.Second)
+		}
+	}
+
+	println("Connected.")
+
+	time.Sleep(2 * time.Second)
+	connectHandler()
+}
+
+func message(msg string) {
+	println(msg, "\r")
+}
